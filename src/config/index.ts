b268e88b65a1d141b067af4e01be66f8e980fa26
ali alzein <aliialzein1@gmail.com>
2026-01-19 16:43:48 +0200

    Added CI

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export default {
+    NODE_ENV: process.env.NODE_ENV || 'development', // Checks if the environment type (e.g., 'production' or 'development') is set; if not, it uses 'development' as default.
+    logDir: 'logs', // Specifies the folder where log files will be saved.
+};
